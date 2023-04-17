d59bb8f7bde0e9ce7af7d623666446f70478895f
Your NameLiaaaaaaaaaaagit config --global user.email you@example.comyllia_vladimirovna@mail.ru <you@example.com>
2023-04-17 22:05:09 +0300

    feat: add part what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+// let date1 = 
+// new Date(2020, 02, 31);
+// {
+//   toString() {
+//       return Date.prototype.toString.call(new Date());
+//   },
+//   [Symbol.toStringTag]: 'Date'
+// };
+
+// console.log ( date1.getUTCMonth());
+
+function getSeason(date) {
+  if (date === undefined) {
+    console.log('zero')
+    return 'Unable to determine the time of year!'
+  }
+
+  if (typeof date !== "object") {
+    console.log('object');
+    return "Invalid date!";
+    
+  }
+
+ try {date.getUTCMonth()} 
+  catch (error) { 
+   return 'Invalid date!';
+  }
+  
+
+  
+  // throw new NotImplementedError('Not implemented');
+
+  let month = date.getUTCMonth();
+  let season = '';
+
+  console.log(month);
+
+  if (month >= 2 && month <= 4) {
+    season = 'spring';
+  } else if (month >= 5 && month <= 7) {
+    season = 'summer';
+  } else if (month >= 8 && month <= 10) {
+    season = 'autumn';
+  } else {
+    season = 'winter';
+  }
+  return season
 }
+
+// console.log(getSeason(date1 ))
 
 module.exports = {
   getSeason
